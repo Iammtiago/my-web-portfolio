60c730af2ac6dce63cbd5aa62edf42338df52a9d
Tiago <santiagoAndres79@outlook.es>
2024-06-14 14:10:55 -0400

    fix vercel?

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(express.static(path.join(__dirname)));
 app.use('/src', express.static(path.join(__dirname, 'src')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/homee', (req, res) => {
     res.status(200).json('¡Bienvenido! Tu aplicación funciona correctamente.');
 });
