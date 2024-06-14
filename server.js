8db5305a5095856b7c72209273f77caf8f40de23
Tiago <santiagoAndres79@outlook.es>
2024-06-14 14:26:53 -0400

    test /

--- server.js
+++ server.js
@@ -24,12 +24,13 @@ app.get('/new-route', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-// app.get('/', (req, res) => {
+// app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/homee', (req, res) => {
     res.status(200).json('¡Bienvenido! Tu aplicación funciona correctamente.');
