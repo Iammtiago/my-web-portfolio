6dd81d12b128ef11d11d5fe39fc70bd00c31d3c7
Tiago <santiagoAndres79@outlook.es>
2024-06-14 14:35:06 -0400

    finish test

--- server.js
+++ server.js
@@ -24,16 +24,12 @@ app.get('/new-route', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/homee', (req, res) => {
-    res.status(200).json('¡Bienvenido! Tu aplicación funciona correctamente.');
+    res.status(200).json('¡Bienvenido a Home Kbrn!');
 });
 
 app.listen(port, () => {
