d5aa57e95878be71c106eafca136fbe094becb12
Tiago <santiagoAndres79@outlook.es>
2024-06-14 22:27:14 -0400

    add endpoints...

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/homee', (req, res) => {
+app.get('/kbrn', (req, res) => {
+    res.status(200).send("what's up, nigga!?")
+});
+
+app.get('/home', (req, res) => {
     res.status(200).json('¡Bienvenido a Home Kbrn!');
 });
 
