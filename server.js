ac271beb6de6263d8d922e6409e42fa6ec43574b
Tiago <santiagoAndres79@outlook.es>
2024-09-10 03:17:44 -0400

    arreglando las rutas, v1.1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 // Habilitar CORS
 app.use(cors());
