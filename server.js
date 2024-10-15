7bdc930d4db7f95b841836d7e48c307d1a6c96e7
Tiago <santiagoAndres79@outlook.es>
2024-10-14 20:05:16 -0400

    interfaz ux funcionando, design perso. todo funcional

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/api/sendmail', (req, res) => {
+    res.json({message: "ok"})
+});
+
 app.get('/kbrn', (req, res) => {
     res.status(200).send("what's up, nigga!?")
 });
